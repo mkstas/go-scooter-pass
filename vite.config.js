018d846fc9c54e2829238873146bec9fc66aed00
mkstas <makarov.stn@gmail.com>
2024-05-08 18:36:41 +0300

    feat: format code, delete typescript

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import Inspect from 'vite-plugin-inspect';
-import checker from 'vite-plugin-checker';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default {
   plugins: [
     Inspect(),
-    checker({
-      typescript: true,
-    }),
     ViteImageOptimizer({
       png: {
         quality: 100,
